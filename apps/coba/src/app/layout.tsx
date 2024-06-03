78d54ce11141bdcf1502263e09c025bad742bf5c
hans <hans@welinux.cl>
2024-06-02 21:53:21 -0400

    Cambia font

--- apps/coba/src/app/layout.tsx
+++ apps/coba/src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Coba</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
+          rel="stylesheet"
+        />
       </head>
       <body>
         <Header />
