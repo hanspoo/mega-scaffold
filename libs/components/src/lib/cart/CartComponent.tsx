6e62d64aed19ecc137514197a570ad6b05e3985d
hans <hans@welinux.cl>
2024-06-07 18:41:04 -0400

    vaciar shopping cart

--- libs/components/src/lib/cart/CartComponent.tsx
+++ libs/components/src/lib/cart/CartComponent.tsx
@@ -10,17 +10,17 @@ export function CartComponent({ cerrar }: { cerrar: () => void }) {
     dispatch(emptyCart());
   };
   return (
-    <div className="bg-sky-500 text-black border-2 shadow-lg -z-1000 p-4 min-h-80 min-w-60 absolute top-8 -right-10 rounded-sm">
+    <div className="bg-sky-100 text-black border-2 shadow-lg z-[1000] p-4 min-h-80 min-w-80 absolute top-8 -right-10 rounded-sm">
       <h2 className="text-2xl mb-2">Shopping Cart</h2>
       {cart!.items.length === 0 && <p>No hay productos</p>}
       {cart && (
-        <table>
+        <table className="mb-20">
           {cart.items.map((item) => (
             <CartItemList item={item} />
           ))}
         </table>
       )}
-      <div className="mt-3">
+      <div className="mt-6 absolute bottom-4">
         {cart!.items.length > 0 && (
           <button className="btn mr-1" onClick={vaciar}>
             Vaciar
