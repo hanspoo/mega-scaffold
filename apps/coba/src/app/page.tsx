252fde9bdece6a60d9f65e7f4d19c743d56bd7f7
hans <hans@welinux.cl>
2024-07-03 09:45:53 -0400

    Grabando denuncia

--- apps/coba/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Provider } from 'react-redux';
-import './global.css';
-import { NeumaticosContainer } from '@coba/components';
-
-export default function Index() {
-  return <NeumaticosContainer />;
-}
